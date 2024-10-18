28e3820e2e205a1d526d7a3be3ad3393e5c03741
Deltell2 <adrideltelllopez12@gmail.com>
2024-10-18 09:01:28 +0200

    Commit inicial

---
+++ src/main/java/org/und4/Main.java
@@ -0,0 +1,10 @@
+package org.und4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("DIVISIÓN");
+        int Paco= 45;
+        int Alba= 15;
+        System.out.println("la mayor edad es= "+Math.max(Paco,Alba));
+        }
+    }
---
+++ src/main/java/org/und4/Test.java
@@ -0,0 +1,9 @@
+package org.und4;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Holaaaaaaaa");
+    }
+}
