d5749275b8a388e9e642f25200d4a3e065b0baee
Adrián Deltell López <adrideltelllopez12@gmail.com>
2024-10-21 13:36:48 +0200

    Add files via upload

---
+++ src/main/java/org/und4/Ejercicio1.java
@@ -0,0 +1,11 @@
+package org.und4;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int dias = 365;
+        int horas =  dias*24;
+        int minutos = horas*60;
+        int segundos = minutos*60;
+        System.out.println("Un año tiene "+horas+" horas, "+minutos+" minutos y "+segundos+" segundos.");
+    }
+}
