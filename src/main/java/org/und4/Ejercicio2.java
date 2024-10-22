92d970621f13f2b38a28e197f8b2d86c7b11794d
Deltell2 <adrideltelllopez12@gmail.com>
2024-10-22 12:30:48 +0200

    Ejercicio 2

---
+++ src/main/java/org/und4/Ejercicio2.java
@@ -0,0 +1,24 @@
+package org.und4;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        String n;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int number = entrada.nextInt();
+        System.out.println((number*1));
+        System.out.println((number*2));
+        System.out.println((number*3));
+        System.out.println((number*4));
+        System.out.println((number*5));
+        System.out.println((number*6));
+        System.out.println((number*7));
+        System.out.println((number*8));
+        System.out.println((number*9));
+        System.out.println((number*10));
+    }
+
+}
