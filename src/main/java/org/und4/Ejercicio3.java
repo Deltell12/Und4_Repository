5e58b8fc4b735addeb712004a05ba051afff746b
Deltell2 <adrideltelllopez12@gmail.com>
2024-10-22 13:19:52 +0200

    Ejercicios

---
+++ src/main/java/org/und4/Ejercicio3.java
@@ -0,0 +1,9 @@
+package org.und4;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int j= -2;
+        boolean b= (j>'0') && (1/(j+2)>10);
+        System.out.println("El resultado es: "+b);
+    }
+}
---
+++ src/main/java/org/und4/Ejercicio5.java
@@ -0,0 +1,15 @@
+package org.und4;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int cont= 10;
+        int limite=20;
+        System.out.println((cont==0)&&(limite<20));
+        System.out.println((limite>=0)||(cont<5));
+       // System.out.println(((limite/(cont-10))>7)||(limite<20));
+        System.out.println((limite<=20)||((limite/(cont-10))>7));
+       // System.out.println(((limite/(cont-10)>7))&&(limite<0));
+        System.out.println((limite<0)&&((limite/(cont-10))>7));
+
+    }
+}
