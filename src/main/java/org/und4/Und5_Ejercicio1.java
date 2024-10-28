149cc055ae82b15892a68e2288b246360f55a9fc
Deltell2 <adrideltelllopez12@gmail.com>
2024-10-28 13:21:02 +0100

    Ejercicios

---
+++ src/main/java/org/und4/Und5_Ejercicio1.java
@@ -0,0 +1,14 @@
+package org.und4;
+
+import java.util.Scanner;
+
+public class Und5_Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese su edad: ");
+        int edad = entrada.nextInt();
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        }
+    }
+}
