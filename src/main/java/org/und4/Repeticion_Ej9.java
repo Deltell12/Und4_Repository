c3b5bc378c525dce9a9135752986a5ae3f9754f8
Deltell2 <adrideltelllopez12@gmail.com>
2024-11-12 13:42:19 +0100

    Ejercicios

---
+++ src/main/java/org/und4/Repeticion_Ej9.java
@@ -0,0 +1,19 @@
+package org.und4;
+
+import java.util.Scanner;
+
+public class Repeticion_Ej9 {
+    public static void main(String[] args) {
+        int i;
+        int suma=0;
+        int mult=1;
+        for (i = 1; i <=10; i++) {
+            suma = suma + i;
+        }
+        for (i=mult; i<=10; i++){
+            mult = mult*i;
+        }
+        System.out.println(suma);
+        System.out.println(mult);
+    }
+}
