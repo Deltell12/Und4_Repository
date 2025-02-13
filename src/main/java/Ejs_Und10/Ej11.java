455ccbd33a37e939f4b7c469697043ea557a5d4b
Deltell GIT <adrideltelllopez12@gmail.com>
2025-02-13 11:08:01 +0100

    Ejercicios

--- src/main/java/Ejs_Und10/Ej11.java
+++ src/main/java/Ejs_Und10/Ej11.java
@@ -1,5 +1,64 @@
 package Ejs_Und10;
 
+import java.util.*;
+
 public class Ej11 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws Exception {
+        HashSet<Integer> conjunto = new HashSet<>();
+
+        rellenarHashSet(conjunto);
+
+        System.out.println(conjunto);
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+        rellenarArrayList(numeros);
+
+        try {
+            System.out.println(interseccion(numeros, conjunto));
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static HashSet<Integer> rellenarHashSet(HashSet<Integer> conjunto) {
+
+        for (int i = 0; i < 1+Math.random()*10; i++) {
+            conjunto.add((int) (1+Math.random()*100));
+        }
+        return conjunto;
+    }
+
+    public static ArrayList<Integer> rellenarArrayList(ArrayList<Integer> numeros) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero=0;
+
+        do {
+            try {
+                System.out.print("Introduce un número: ");
+                numero = sc.nextInt();
+                numeros.add(numero);
+            }
+            catch (InputMismatchException e) {
+                System.out.println("EL número debe ser entero");
+                sc.nextLine();
+            }
+        }
+        while (numero>=0);
+
+        return numeros;
+    }
+
+    public static HashSet<Integer> interseccion(ArrayList<Integer> numeros, HashSet<Integer> conjunto) throws Exception{
+
+
+        conjunto.retainAll(numeros);
+
+        if (conjunto.isEmpty()) {
+            throw new Exception("No hay ningún número en común.");
+        }
+
+        return conjunto;
+    }
 }
--- src/main/java/Ejs_Und10/Ej5.java
+++ src/main/java/Ejs_Und10/Ej5.java
@@ -6,38 +6,56 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ej5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws posicion2EsZero {
 
-        int tamanio=10;
+        int tamanio = 10;
 
         int[] array = new int[tamanio];
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random()*10);
+            array[i] = (int) (Math.random() * 10);
+            array[3] = 0;
         }
 
-        System.out.println("Array: "+Arrays.toString(array));
-
-        System.out.println("El resultado de la division es: "+Excepciones(array));
+        System.out.println("Array: " + Arrays.toString(array));
+        try {
+            System.out.println("El resultado de la division es: " + preguntarPosiciones(array));
+        }
+        catch (posicion2EsZero e) {
+            System.out.println(e.getMessage());
+            System.out.println(0);
+        }
     }
-    public static double Excepciones (int[] array) {
+
+    public static double preguntarPosiciones(int[] array) throws posicion2EsZero {
 
         double division = 0;
-        try{
-            Scanner sc=new Scanner(System.in);
+        try {
+            Scanner sc = new Scanner(System.in);
             System.out.print("Introduce una posicion: ");
-            int posicion1=sc.nextInt();
+            int posicion1 = sc.nextInt();
 
             System.out.print("Introduce otra posicion: ");
-            int posicion2=sc.nextInt();
+            int posicion2 = sc.nextInt();
 
-            division= (double) array[posicion1] /array[posicion2];
+            division = (double) array[posicion1 - 1] / array[posicion2 - 1];
 
+            if (array[posicion2-1] == 0) {
+                throw new posicion2EsZero("Error, no se puede dividir entre 0.");
+            }
 
+        } catch (InputMismatchException e)  {
+            System.out.println("Error, debes de poner un número entero.");
+            return 0;
         }
-        catch(InputMismatchException | ArrayIndexOutOfBoundsException | ArithmeticException e){
-            System.out.println("Error");
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Error, te has pasado del límite.");
         }
         return division;
     }
 }
+ class posicion2EsZero extends Exception{
+    public posicion2EsZero(String mensaje) {
+        super(mensaje);
+    }
+ }
