2bc6046698fe88e1c173162b385693bbf96134f4
Deltell GIT <adrideltelllopez12@gmail.com>
2025-03-20 13:44:37 +0100

    Ejercicios

--- src/main/java/Und8_Parte2/Ejs/Ej8/Empresa.java
+++ src/main/java/Und8_Parte2/Ejs/Ej8/Empresa.java
@@ -5,4 +5,26 @@ import java.util.ArrayList;
 public class Empresa {
     private String nombre;
     private ArrayList<Empleado> empleados;
+
+    public Empresa(String nombre, ArrayList<Empleado> empleados) {
+        this.nombre = nombre;
+        this.empleados = empleados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
 }
