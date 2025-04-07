f4e1484e6217c1f976a5186324e8539dfd841f2e
Deltell GIT <adrideltelllopez12@gmail.com>
2025-04-07 13:48:42 +0200

    Ejercicios

---
+++ src/main/java/Ejs_Und11/FicherosBinarios/Ej8/FicherosBinario_Ej8.java
@@ -0,0 +1,27 @@
+package Ejs_Und11.FicherosBinarios.Ej8;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FicherosBinario_Ej8 {
+    private static final String nombre = "texto.dat";
+    private static final String ruta = Paths.get("src", "main", "java", "Ejs_Und11", "FicherosBinarios", "Ej8").toString();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String texto = "";
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ruta))) {
+            do{
+                System.out.print("Ingresa : ");
+            }
+        }catch (IOException e){
+
+        }
+    }
+}
--- src/main/java/Ejs_Und11/JSON/GestorJugadores.java
+++ src/main/java/Ejs_Und11/JSON/GestorJugadores.java
@@ -1,10 +1,19 @@
 package Ejs_Und11.JSON;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
-
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class GestorJugadores {
+    private static Jugador [] arrayJugadoresExistentes;
+    private static List<Jugador> listaJugadores;
+
     private static final String Fichero = "jugadores.json";
     private static final String ruta = Paths.get("src", "main", "java", "Ejs_Und11", "JSON").toString();
 
@@ -31,5 +40,9 @@ public class GestorJugadores {
         }
         while (opcion != 6);
     }
-    public static void escribirJSON() {}
+    public static void escribirJSON() throws Exception {
+        Jugador j1 = new Jugador("Steve", 15, 15000);
+        Jugador j2 = new Jugador("Alex", 10, 12420);
+        listaJugadores = Arrays.asList(j1, j2);
+    }
 }
